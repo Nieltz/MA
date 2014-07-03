2ce894ce9cdf83859332f081f2e42d753f7ecd2c
Nils Larcher <d1136@pc216.(none)>
2014-07-03 14:42:47 +0200

    edited matlab workspaces

--- RecordedData/createWs.m
+++ RecordedData/createWs.m
@@ -6,7 +6,7 @@ fileList=dir(fullfile('.','*.txt'));
 for ii= 1:length(fileList) % start at 3 because first2 entries are . and ..
     fileName=fileList(ii).name;
     [lines, accData, gyroData]=readSensData(fileName,'Car');
-    [aX, aY, aZ, gX, gY, gZ] = processSensData(lines, accData(2), gyroData(3),'Car');
+    [aZ, aY, aX, gX, gY, gZ] = processSensData(lines, accData(2), gyroData(3),'Car');
     save([fileName,'.mat'],'aX','aY','aZ','gX','gY','gZ')
     clear lines 
     
