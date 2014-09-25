b55524000be736544d62a3b301226a7d7d57d243
Nieltz <nightfighter1@gmx.net>
2014-09-25 11:38:04 +0200

    update shiftdet

--- testShiftDetection.m
+++ testShiftDetection.m
@@ -1,4 +1,4 @@
-function [ falseDetection, missDetection ] = testShiftDetection()
+function [ falseDetection, missDetection, missDetectRate, falseDetectRate ] = testShiftDetection()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,7 +6,7 @@ addpath ..
 
 %fileList=dir(fullfile('.','*.mat'));
 ts = 0.001;
-detectorThresholds=[20:40];
+detectorThresholds=[500:50:800];
 load('labeledShifts2.mat');
 [excelShifts, excelText] =  xlsread('shifts.xlsx','shiftsForMatlab');
 [a textLineLength]=size(excelText);
@@ -14,20 +14,25 @@ load('labeledShifts2.mat');
 readGearDatabase;
 
 %select desired files form Database
-startFileNo = 12;
-endFileNo =  12;
+startFileNo = 35;
+endFileNo =  37;
 runList = [startFileNo:endFileNo]; % set a vector with desired samples here!
 %load shifts;
 falseDetection = zeros(length(detectorThresholds),length(runList));
 missDetection = zeros(length(detectorThresholds),length(runList));
 
+    totalNoOfRealShifts =zeros(length(detectorThresholds),1);
+    missDetectRate = zeros(length(detectorThresholds),1);
+    falseDetectRate = zeros(length(detectorThresholds),1);
+
+
 for testRuns=1:length(detectorThresholds);
-    
+
     checkDetected =0;
     %fileList = {'1.txt.mat','2.txt.mat','3.txt.mat','4.txt.mat','5.txt.mat','R.txt.mat','1-2.txt.mat','1bis5.txt.mat'};
     
     % create Shift detector object
-    shiftDetectorObj = shiftDetector(4,detectorThresholds(testRuns));
+    shiftDetectorObj = shiftDetector(detectorThresholds(testRuns),25);
     
     % parse name
     ind=0;
@@ -42,22 +47,34 @@ for testRuns=1:length(detectorThresholds);
         aFX = slidingWindowFilter(aX,300);
         aFY = slidingWindowFilter(aY,300);
         aFZ = slidingWindowFilter(aZ,300);
+        
+%        [shift, lklhd] = shiftDetectorObj.shiftDetectionCa_Cfar(aX,aY,aZ,10,21,5 );
         for ii = 300:length(aX)
             
             % Shift Detection
-            [shift(ii), lklhd(ii)] = shiftDetectorObj.shiftDetection2(aFX(ii),aFY(ii),aFZ(ii),ii);%(aTPX(ii)-aTPX(ii-1)),(aTPY(ii)-aTPY(ii-1)),(aTPZ(ii)-aTPZ(ii-1)),ii);
+            [shift(ii), lklhd(ii)] = shiftDetectorObj.shiftDetection2(aX(ii),aY(ii),aZ(ii),ii);%(aTPX(ii)-aTPX(ii-1)),(aTPY(ii)-aTPY(ii-1)),(aTPZ(ii)-aTPZ(ii-1)),ii);
             if (shift(ii-1)== 1 && shift(ii)==0)
                 shifts=shifts+1;
             end
             
         end
+        
+        % Count false and miss detections
         if (shifts-noOfShifts)>0
             falseDetection(testRuns,ind)= (shifts-noOfShifts);
         elseif (shifts-noOfShifts)<0
             missDetection(testRuns,ind) = noOfShifts-shifts;
         end
+        
+        %sum Shifts from Excel
+        totalNoOfRealShifts(testRuns) = noOfShifts +totalNoOfRealShifts(testRuns);
+       %totalNoOfDetectedShifts(testRuns) = totalNoOfDetectedShifts(testRuns);
         clear shifts
     end
+    
+    %% Calculate Detection Rates
+    missDetectRate(testRuns) = sum(missDetection(testRuns,:))/totalNoOfRealShifts(testRuns);
+    falseDetectRate(testRuns) = sum(falseDetection(testRuns,:))/totalNoOfRealShifts(testRuns);
     clear shiftDetectorObj
     
 end
